de0a65ccf1af07dce7bef6bbb2f335344e47ce9c
lsisreviving <34271743+vigneshls@users.noreply.github.com>
2023-07-10 12:37:03 +0530

    I have disordered

--- Faceit.py
+++ Faceit.py
@@ -8,12 +8,3 @@ img = tf.keras.Input(shape=(224, 224, 3))
   
 //Preprocess the image for the model //  
 x = tf.keras.layers.Lambda(lambda x: x / 255.0)(img)  
-  
-// Run the image through the model //  
-predictions = model(x)  
-  
-// Find the location of the face in the image //  
-face_locations = tf.where(predictions > 0.5)  
-  
-// Print the location of the face in the image //  
-print(face_locations)  
